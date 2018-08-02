65c839652d014390f591e0ae64ef2a2ad421ee5d
thoo <theinhtay@gmail.com>
2018-08-02 19:25:45 -0400

    string to float read_csv error

--- src/functions.py
+++ src/functions.py
@@ -1,19 +1,26 @@
 import pandas as pd 
 import os
+import numpy as np 
 
 def process_fun(data):
 	"""This function will return drug name, total drug cost 
-	and total number of prescribers from input data files."""
+	and total number of prescribers from input data files 
+	in descending order by total_cost followed by 'drug_name'."""
 	return (data.groupby('drug_name')
 		.agg({'id':'count', 'drug_cost': 'sum'})
-		.reset_index())
+		.reset_index()
+		.rename(columns={'id':'num_prescriber','drug_cost':'total_cost'})
+		.sort_values(by=['total_cost','drug_name'],ascending=False)
+		)
 
-def read_csv_chunk(filename,chunksize):
-	""" Read csv file in chunks and select the related columns. Count 'drug_name' column for the identical name 
-	and sum the amount of cost from drug_cost column. Return as a data frames."""
-	data_frames=[process_fun(chunk) for chunk in pd.read_csv(filename, chunksize=chunksize,\
-		usecols=['id','drug_name','drug_cost'])]
-	return pd.concat(data_frames)
+def read_csv(filename):
+	""" Read selected columns ['id','drug_name', and 'drug_cost'] from a csv file to memory. 
+	Make sure 'drug_cost' is in float data type. 
+
+	"""
+
+	data=pd.read_csv(filename,usecols=['id','drug_name','drug_cost'],dtype={'drug_cost':np.float64})
+	return data
 
 def make_outputfolder(output_path):
 	#Create an folder for output file if it doesn't exist. 
--- src/pharmacy-counting.py
+++ src/pharmacy-counting.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np 
 import os
 from argparse import ArgumentParser
 from functions import *
@@ -10,7 +11,7 @@ parser = ArgumentParser()
 #parser.add_argument("-f", "--file", dest="myFilenameVariable",help="write report to FILE", metavar="FILE")
 parser.add_argument('-i','--input',dest="input_path", help='Filename and location of the input file.', required=True)
 parser.add_argument('-o','--output' ,dest="output_path",help='Filename and location of the output file.',default="Not_MenTion")
-parser.add_argument('-z','--chunk_size' ,dest="size",help='This is chunk size for the input file. The size of the number of rows to keep on the memory.', default=10**6)
+
 args = parser.parse_args()
 
 
@@ -20,9 +21,7 @@ args = parser.parse_args()
 
 
 def test_run():
-	#For the big data file, we will set chunksize 
-	# that we won't load all data to the memory.
-	chunksize=args.size
+	
 	input_path=args.input_path
 
 	#Set output file path if not defined.
@@ -32,17 +31,14 @@ def test_run():
 		output_path=os.sep.join(input_path.split(os.sep)[0:-2])+os.sep+'output'+os.sep+'top_cost_drug.txt'
 
 	
-	comb_data=read_csv_chunk(input_path,chunksize)
+	data=read_csv(input_path)
 	
-	#Recount for the same id from different chunks. Rename the columns. Sort the result.
-	comb_data_mod=process_fun(comb_data)\
-		.rename(columns={'id':'num_prescriber','drug_cost':'total_cost'})\
-		.sort_values(by=['total_cost','drug_name'],ascending=False)
+	results=process_fun(data)
 
 	make_outputfolder(output_path)
 	
 	#Write a csv file.
-	comb_data_mod.to_csv(output_path,index=False)
+	results.to_csv(output_path,index=False)
 
 if __name__ == '__main__':
 	test_run()
